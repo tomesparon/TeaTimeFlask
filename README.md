b85c5f39cbe92b9738bc7945b00f785443f2802c
Tom Esparon <tespo@btinternet.com>
2018-01-11 18:33:36 +0000

    simpler video processing

---
+++ README.md
@@ -0,0 +1,13 @@
+## Video Streaming with Flask Example
+
+### Description
+Modified to support streaming out with webcams, and not just raw JPEGs.
+
+### Credits
+Most of the code credits to Miguel Grinberg, except that I made a small tweak. Thanks!
+http://blog.miguelgrinberg.com/post/video-streaming-with-flask
+
+### Usage
+1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
+2. Run "python main.py".
+3. Navigate the browser to the local webpage.
--- src/teatime_flask.py
+++ src/teatime_flask.py
@@ -1,14 +1,7 @@
 # camera dependencies
-from importlib import import_module
-import os
 
+from camera import VideoCamera
 from flask import Flask, render_template, request,Response
-
-# import camera driver
-if os.environ.get('CAMERA'):
-    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
-else:
-    from camera import Camera
 
 
 app = Flask(__name__)
@@ -25,17 +18,15 @@ def mainpage():
         return render_template('main_template.html')
 
 def gen(camera):
-    """Video streaming generator function."""
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),
+    return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
