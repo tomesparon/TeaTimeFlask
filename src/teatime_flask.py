a06bd53694d4b8241a11f3446d8b0f55f008cb63
Tom Esparon <tespo@btinternet.com>
2018-01-12 18:57:13 +0000

    form with name custom msg

--- src/teatime_flask.py
+++ src/teatime_flask.py
@@ -1,13 +1,18 @@
+# -*- coding: utf-8 -*-
 
-
-from flask import Flask, render_template, request,Response
-
+from flask import Flask, render_template, request, Response, redirect
+from config import Config
+from forms import LoginForm
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
-    if request.method == 'POST':
+    form = LoginForm()
+    if form.validate_on_submit():
+        text = form.name.data
         #press button via gpio
         from subprocess import Popen,PIPE,STDOUT
         Popen(["python", "piservo.py"],stdout=PIPE)
@@ -17,17 +22,35 @@ def mainpage():
         except ImportError:
             import os
             DEVNULL = open(os.devnull, 'wb')
-
-        text = "Your kettle is on"
+        text = "Your kettle is On, MR " + text
         p = Popen(['espeak', '-b', '1'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
         p.communicate(text)
-
         return render_template('boiling.html')
-        
-    else:
-        return render_template('main_template.html')
+
+    #default
+    return render_template('main_template.html',form=form)
 
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host='127.0.0.1', port=8000, debug=True)
+
+
+    # if request.method == 'POST':
+    #     #press button via gpio
+    #     from subprocess import Popen,PIPE,STDOUT
+    #     # Popen(["python", "piservo.py"],stdout=PIPE)
+    #     # #say message
+    #     # try:
+    #     #     from subprocess import DEVNULL
+    #     # except ImportError:
+    #     #     import os
+    #     #     DEVNULL = open(os.devnull, 'wb')
+
+    #     # text = "Your kettle is on"
+    #     # p = Popen(['espeak', '-b', '1'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+    #     # p.communicate(text)
+
+    #     return render_template('boiling.html')
+        
+    # else:
